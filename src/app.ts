29f99ce133e4c5d86ad100b4b6b23325a38f3e02
Andres Parra <andeveling@gmail.com>
2022-11-03 18:54:33 -0500

    fix/deploy

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,11 @@ const app: Application = express();
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(compression());
-
 app.use(morgan('dev'));
 app.use(cors());
+app.use((_req, res) => {
+  res.send('Hello World!');
+});
 
 routes(app);
 
