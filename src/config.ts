fa0db3c5bcc9fe514181e8e266e2ce4d6ad69ad5
Andres Parra <andeveling@gmail.com>
2022-11-03 18:20:55 -0500

    fix/deploy

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ const config = {
 
   DB_HOST: process.env.DB_HOST ?? 'localhost',
   DB_PORT: process.env.DB_PORT ?? 5432,
-  DB_NAME: process.env.DB_name ?? 'hackaton',
+  DB_NAME: process.env.DB_NAME ?? 'hackaton',
   DB_USER: process.env.DB_USER ?? 'postgres',
   DB_PASSWORD: process.env.DB_PASSWORD ?? 'postgres',
 
